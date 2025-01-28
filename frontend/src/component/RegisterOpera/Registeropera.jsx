c99f72b3509297e1e22c337f0de53d46c9044f8d
Sirapop Sorntad <Sirapop0122@gmail.com>
2025-01-28 16:55:38 +0700

    Fix FileName

--- frontend/src/component/RegisterOpera/Registeropera.jsx
+++ frontend/src/component/RegisterOpera/Registeropera.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Import Link
-import "./Registeropera.css";
+import "./RegisterOpera.css";
 /**/ 
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
