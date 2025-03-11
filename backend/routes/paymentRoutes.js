bb09ce822d11ec4b97d90dc47d493f6516115cd8
Sirapop Sorntad <Sirapop0122@gmail.com>
2025-03-12 02:10:31 +0700

    Add Payment Complete

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const paymentController = require("../controllers/paymentController");
+const upload = require("../middlewares/upload")
 
+router.post("/submit", upload.single("slipImage"), paymentController.submitPayment);
 router.get("/pending", paymentController.getPendingPayment);
 
 module.exports = router;
