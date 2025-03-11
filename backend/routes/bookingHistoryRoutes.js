633911c29d0cff512539e8c52fc1f8484b9dcf0a
Thitipat Charonekitpasuk <thitipat.cha@ku.th>
2025-03-12 00:02:31 +0700

    historymai

--- backend/routes/bookingHistoryRoutes.js
+++ backend/routes/bookingHistoryRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const { getUserBookingHistory, addBookingHistory, confirmBooking} = require("../controllers/bookingHistoryController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
+const bookingHistoryController = require("../controllers/bookingHistoryController");
 
 // ✅ ดึงประวัติการจองของผู้ใช้
-router.post("/", protect, getUserBookingHistory);
+router.get("/", bookingHistoryController.getUserBookingHistory);
 
 // ✅ บันทึกการจอง
 router.post("/booking-history", protect, addBookingHistory);
