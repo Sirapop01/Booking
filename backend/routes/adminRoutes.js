b0de0ebbe402bdab6e40b6072bc9d0e47b7e6588
Sirapop Sorntad <Sirapop0122@gmail.com>
2025-03-14 01:38:24 +0700

    Fix

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const { registerAdmin } = require("../controllers/adminController");
 const upload = require("../middlewares/upload"); // ใช้ Multer สำหรับอัปโหลดไฟล์
-const { protect, superAdminAuth } = require("../middlewares/authMiddleware");
-
 const router = express.Router();
 
 // ✅ สมัคร Admin (เฉพาะ SuperAdmin)
-router.post("/register", protect, superAdminAuth, upload.single('profileImage'), registerAdmin);
+router.post("/register", upload.single('profileImage'), registerAdmin);
 
 module.exports = router;
